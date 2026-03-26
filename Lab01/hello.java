adbe8a1b91caab092285df8fe73d18f3ceb3b69b
ogneyu <ngouyen06122006@gmail.com>
2026-03-26 19:21:42 +0700

    test

---
+++ Lab01/hello.java
@@ -0,0 +1,15 @@
+public class hello {
+    static void main() {
+        System.out.print("chuoi la ban");
+        System.out.println("");
+        System.out.print("\tuieen");
+        System.out.println();//sout
+        //ctr+D
+        //khai bao khoi tao bien: kieudulieu + tenbien=gtri
+        //khai bao hang: final kieudulieu + tenbien = gtri
+        float diemtoan=9.5f;
+        int tuoi=10;
+        System.out.println(tuoi);
+        double k= (double) tuoi*diemtoan;
+    }
+}
