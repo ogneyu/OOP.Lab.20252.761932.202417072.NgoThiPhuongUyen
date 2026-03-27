108cbba2719cb763f7326ae829d0f209aaf3c5e8
ogneyu <ngouyen06122006@gmail.com>
2026-03-27 23:43:50 +0700

    Submit Lab01

---
+++ Lab01/BT2/2.2.2/FirstDialog.java
@@ -0,0 +1,9 @@
+package BT2;
+
+import javax.swing.JOptionPane;
+public class FirstDialog {
+    public static void main(String[] args) {
+        JOptionPane.showMessageDialog(null,"Hello word! How are you?");
+        System.exit(0);
+    }
+}
---
+++ Lab01/BT2/2.2.3/HelloNameDialog.java
@@ -0,0 +1,12 @@
+package BT2;
+// Example 3: BT2.HelloNameDialog.java
+import javax.swing.JOptionPane;
+public class HelloNameDialog {
+    static void main() {
+        String result;
+        result = JOptionPane.showInputDialog("Please enter your name:");
+        JOptionPane.showMessageDialog(null, "Hi " + result + "!");
+        System.exit(0);
+        }
+}
+
