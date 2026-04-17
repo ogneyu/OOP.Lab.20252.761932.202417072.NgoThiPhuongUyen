8c124e5042e55feb060209cecac3ebfd0f87f2fe
ogneyu <ngouyen06122006@gmail.com>
2026-04-18 00:57:15 +0700

    add store

---
+++ Lab03/SourceCode/StoreTest.java
@@ -0,0 +1,18 @@
+package AimsProject;
+
+public class StoreTest {
+    public static void main(String[] args) {
+
+        Store store = new Store();
+
+        DigitalVideoDisc dvd1 = new DigitalVideoDisc("The Lion King");
+        DigitalVideoDisc dvd2 = new DigitalVideoDisc("Star Wars");
+        DigitalVideoDisc dvd3 = new DigitalVideoDisc("Aladin");
+
+        store.addDVD(dvd1);
+        store.addDVD(dvd2);
+        store.addDVD(dvd3);
+
+        store.removeDVD(dvd2);
+    }
+}
